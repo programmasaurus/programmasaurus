63d8f1a886258e1344c421b9f4a0c967d41fa37f
brianstanwyck <stanwyck@bignerdranch.com>
2013-05-02 16:36:45 -0400

    Adds factory for synsets

---
+++ app/models/synset_builder.rb
@@ -0,0 +1,15 @@
+class SynsetBuilder
+  def self.create(word: nil, synset_id: nil, wordnet_synset: nil)
+    # find by word and wordnet ID if a synset object is not provided
+    if wordnet_synset.blank?
+      wordnet_homographs = Wordnet.instance.find(lemma)
+      wordnet_synset = wordnet_homographs.find { |homograph| homograph.synset_id == synset_id }
+    end
+
+    Synset.where(
+      gloss: wordnet_synset.gloss,
+      part_of_speech: wordnet_synset.synset_type.to_s,
+      synset_id: wordnet_synset.synset_id
+    ).first_or_create!
+  end
+end
