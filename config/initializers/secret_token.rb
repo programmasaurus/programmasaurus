9306a55733873b2ae01cfcc389a6c96dbfb03bbd
brianstanwyck <stanwyck@bignerdranch.com>
2013-05-02 11:15:29 -0400

    Real initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Programmasaurus::Application.config.secret_key_base = 'ac5a9ad3e62ab3c45d46367a732b15f6ef5c6741517c9192454d9640e3a9b476d9e5666d2bcd5d2830b0dc29d780bf13539f2a9281c20e76cb1b0a7a14c14da3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Programmasaurus::Application.config.session_store :cookie_store, key: '_programmasaurus_session'
