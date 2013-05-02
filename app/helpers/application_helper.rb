f3d2ab49109bc02d3616bcedb89ecd5402fa572f
Brandon Beacher <brandon.beacher@gmail.com>
2013-05-02 19:14:08 -0400

    Dress up the disambiguation page a bit.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  DISAMBIGUATION_COLOR_CLASSES = {
+    0 => "green-sea",
+    1 => "nephritis",
+    2 => "belize-hole",
+    3 => "wisteria",
+    4 => "midnight-blue",
+    5 => "orange",
+    6 => "pumpkin",
+    7 => "pomegranate",
+    8 => "asbestos" }
+  
+  def disambiguation_color_class(index)
+    DISAMBIGUATION_COLOR_CLASSES[index]
+  end
+
 end
