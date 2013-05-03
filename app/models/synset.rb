845aec7b7356e5f2e0bf3a70ef183f29b91f17c0
brianstanwyck <stanwyck@bignerdranch.com>
2013-05-03 16:34:47 -0400

    More descriptive name for  on Synset

--- app/models/synset.rb
+++ app/models/synset.rb
@@ -1,6 +1,9 @@
 class Synset < Struct.new(:wordnet_synset)
   delegate :gloss, to: :wordnet_synset
-  delegate :words, to: :wordnet_synset
+
+  def lemmas
+    wordnet_synset.words
+  end
 
   def self.from_id(synset_id)
     Synset.new(Wordnet.instance.find_by_synset_id(synset_id))
