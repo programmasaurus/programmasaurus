0ac6aae7572c1b7ce42cd906a94f52ab3be0220a
brianstanwyck <stanwyck@bignerdranch.com>
2013-05-03 17:55:15 -0400

    Vocabulary/word builder controller code

--- app/controllers/vocabularies_controller.rb
+++ app/controllers/vocabularies_controller.rb
@@ -1,2 +1,16 @@
 class VocabulariesController < ApplicationController
+  before_filter :require_user!
+
+  def create
+    vocabulary_params = params[:vocabulary].permit(:title)
+    vocabulary = current_user.vocabularies.build(vocabulary_params)
+
+    word_params = params[:word].permit(:lemma, :synset_id)
+    word = vocabulary.words.build(word_params)
+
+    vocabulary.save!
+    word.save!
+
+    redirect_to :back
+  end
 end
