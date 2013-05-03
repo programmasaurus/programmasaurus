ad362d4f18671987a41ce238a79f344f4fe4776f
brianstanwyck <stanwyck@bignerdranch.com>
2013-05-03 17:01:09 -0400

    Adds for_lemma scope to vocabularies

--- app/models/vocabulary.rb
+++ app/models/vocabulary.rb
@@ -1,4 +1,9 @@
 class Vocabulary < ActiveRecord::Base
   belongs_to :user
   has_many :words
+
+  scope :for_lemma, ->(synset_id, lemma) do
+    words = Word.for_lemma(synset_id, lemma)
+    includes(:words).where(words: {id: words})
+  end
 end
