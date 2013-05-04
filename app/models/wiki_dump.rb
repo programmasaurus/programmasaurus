1aa0a920747c590ab7dee174b93abd2f9d823c85
brianstanwyck <stanwyck@bignerdranch.com>
2013-05-04 01:46:29 -0400

    Working WikiDump model
    * Takes a synset as an input, and creates a set of suggestions based on
      wikipedia links for each lemma associated to the synset.

--- app/models/wiki_dump.rb
+++ app/models/wiki_dump.rb
@@ -1,8 +1,31 @@
 require 'open-uri'
 
-class WikiDump < Struct.new(:url)
+class WikiDump < Struct.new(:synset)
+  def create_suggestions!
+    synset.words.each do |word|
+      dest_synsets = WordCrawl.new(word).synsets
+      p dest_synsets.map(&:words)
+      dest_synsets.each do |dest_synset|
+        Suggestion.create!(
+          source_synset_id: synset.synset_id,
+          dest_synset_id:   dest_synset.synset_id
+        )
+      end
+    end
+  end
+end
+
+class WordCrawl < Struct.new(:word)
   def doc
-    @doc ||= Nokogiri::HTML(open(url))
+    @doc ||= Nokogiri::HTML(open(source_url))
+  end
+
+  def wikified_word
+    word.titleize.gsub(/\s+/, '_')
+  end
+
+  def source_url
+    "http://en.wikipedia.org/wiki/#{wikified_word}"
   end
 
   def links
@@ -16,12 +39,14 @@ class WikiDump < Struct.new(:url)
       url =~ %r(^/wiki/\w+$)
     end.map do |url|
       url.gsub(%r(^/wiki/), '').gsub('_', ' ').downcase
-    end
+    end.uniq
   end
 
   def synsets
     @synsets ||= wiki_words.map do |word|
-      Wordnet.instance.find(word)
+      print '.'
+      homographs = Wordnet.instance.find(word)
+      homographs.senses.first if homographs
     end.compact
   end
 end
