78b96494686bfb944bdd826c7df469cd8c48fc56
Brandon Beacher <brandon.beacher@gmail.com>
2013-05-04 16:05:44 -0400

    Allow direct editing of vocabularies.

--- app/controllers/synsets_controller.rb
+++ app/controllers/synsets_controller.rb
@@ -3,6 +3,7 @@ class SynsetsController < ApplicationController
 
   def show
     @synset = Synset.from_id(params[:id])
-    @homograph = Homograph.lookup(params[:origin_lemma]) if params[:origin_lemma].present?
+    @homograph = Homograph.lookup(params[:origin_lemma])
+    @word = Word.new(lemma: params[:origin_lemma], synset_id: @synset.id)
   end
 end
